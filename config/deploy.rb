ca6387a9c0851f4c33c4aa26748404c75e8db0b2
mike yurchenkov <mikesehse@gmail.com>
2017-02-27 23:25:41 +0300

    puma fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :ssh_options, { :forward_agent => true }
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", 'config/puma.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,20 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS").to_i { 5 }.to_i
 threads threads_count, threads_count
 
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
+# Set up socket location
+bind "unix://tmp/puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
@@ -40,7 +54,9 @@ preload_app!
 # cannot share connections between processes.
 #
 on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
 
 # Allow puma to be restarted by `rails restart` command.
