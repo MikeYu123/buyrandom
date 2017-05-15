643beac321a67965db633e23a6cef8e8218b685d
Mike Yurchenkov <mikesehse@gmail.com>
2017-05-15 23:14:40 +0300

    some admin fixes

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,46 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
+menu :if => -> { current_user.admin? }
 permit_params :email, :password, :password_confirmation, :balance, :role, :avatar_url
+
+form do |f|
+  f.inputs do
+    f.input :email
+    f.input :password
+    f.input :password_confirmation
+    f.input :balance
+    f.input :role
+    f.input :avatar_url
+  end
+  f.submit
+end
+
+controller do
+  before_filter :admin_filter
+
+  def admin_filter
+    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+  end
+end
+
+index do
+  column :id
+  column :email
+  column :role
+  column :balance
+  column :avatar_url
+  column :uid
+end
+
+show do
+  row :email
+  row :role
+  row :balance
+  row :avatar_url
+  row :created_at
+  row :updated_at
+end
 #
 # or
 #
