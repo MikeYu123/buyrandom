36c9fcacb40686a0320d38b33305a6fe3273ff93
Mike Yurchenkov <mikesehse@gmail.com>
2017-05-15 19:01:12 +0300

    fixed bug when no current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_admin_user!
-    current_user.can_see_admin?
+    current_user&.can_see_admin?
   end
 end
