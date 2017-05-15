358815e1f4a1e20cd167594a6d4290984bdee5aa
Mike Yurchenkov <mikesehse@gmail.com>
2017-05-15 19:59:52 +0300

    added lowercase indexing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
 	audited
-	scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%")}
+	scope :by_name, ->(name) { where('lower(name) LIKE ?', "%#{name.downcase}%") }
 end
